439f7db1d7232ee6d42a48746ae31e0f38f0ffa8
rizwana573 <mehar.rizwana573@gmail.com>
2026-01-22 10:05:15 +0530

    update content stck.js

--- src/contentstack.js
+++ src/contentstack.js
@@ -4,11 +4,6 @@ const Stack = Contentstack.Stack({
   api_key: import.meta.env.VITE_CS_API_KEY,
   delivery_token: import.meta.env.VITE_CS_DELIVERY_TOKEN,
   environment: import.meta.env.VITE_CS_ENVIRONMENT,
-  live_preview: {
-    enable: true,
-    preview_token: import.meta.env.VITE_CS_PREVIEW_TOKEN,
-    host: "rest-preview.contentstack.com",
-  },
 });
 
 export default Stack;
